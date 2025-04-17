6b026d5f27f74de50ba9e941c7760023a160cfc1
Nataliia Ivchenko <nataliia.ivchenko.ua@gmail.com>
2025-04-17 23:26:32 +0200

    add first test

---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,7 @@
+describe('home', () => {
+  it('the h1 contains the correct text', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('h1')
+    cy.contains('Testing Next.js Applications with Cypress')
+  })
+})
